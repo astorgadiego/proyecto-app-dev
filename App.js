da0ce236115d1a69585de7f04ea47955999e3f28
Diego Astorga <diego.astorga98@gmail.com>
2023-04-14 23:52:53 -0300

    Primer Commit, agregue el Hola Coder!

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Coder! Este es el Proyecto de Diego</Text>
       <StatusBar style="auto" />
     </View>
   );
